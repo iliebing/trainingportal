3b93e1d930da25cd8a6018f3f19d7cfe6d5fea18
Andreas Beer <andreas.beer@int.sevenval.com>
2018-06-08 12:31:35 +0200

    add js to index.html

--- js/main.js
+++ js/main.js
@@ -4,8 +4,23 @@
 
     var Post = globalNS.Post;
 
+    function extract(key) {
+        return function (entry) {
+            return entry.fields[key]
+        }
+    }
 
-    
+
+    // globalNS.contentfulApi.create.post(post.createEntry())
+    // .then(console.log)
+    // .catch(console.warn);
+
+    globalNS.contentfulApi.select.allPosts()
+        .then(e => console.log(e.items.map(extract('title'))))
+        .catch(console.warn);
+
+
+
 
 
 
